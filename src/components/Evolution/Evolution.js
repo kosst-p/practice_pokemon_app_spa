9e176d81ab3e56ded0a266791b5dca1bf0368577
kosst-p <pirozhkovks@yandex.ru>
2020-04-22 17:17:11 +0300

    create detail page with style

--- src/components/Evolution/Evolution.js
+++ src/components/Evolution/Evolution.js
@@ -2,54 +2,54 @@ import React, { useEffect, useState } from "react";
 import classes from "./Evolution.module.css";
 import Axios from "axios";
 
-const Evolution = props => {
+const Evolution = (props) => {
   const evolutionURL = props.evolution;
   const [evolutionChain, setEvolutionChain] = useState([
     [
       {
         name: "",
-        url: ""
-      }
+        url: "",
+      },
     ],
     [
       {
         name: "",
-        url: ""
+        url: "",
       },
       {
         name: "",
-        url: ""
-      }
+        url: "",
+      },
     ][
       {
         name: "",
-        url: ""
+        url: "",
       }
-    ]
+    ],
   ]);
 
   useEffect(() => {
     const fetchData = async () => {
       await Axios({
         method: "GET",
-        url: evolutionURL
-      }).then(result => {
+        url: evolutionURL,
+      }).then((result) => {
         console.log("--------------");
         console.log("массив для обхода - ", result.data.chain);
         console.log("--------------");
 
         let resultUPD = [];
         resultUPD.push([result.data.chain.species]);
-        const getEvolves = object => {
+        const getEvolves = (object) => {
           if (Array.isArray(object.evolves_to) && !object.evolves_to.length) {
             return object.evolves_to;
           }
-          const resultNew = object.evolves_to.map(evolves =>
+          const resultNew = object.evolves_to.map((evolves) =>
             getEvolves(evolves)
           );
           return [...object.evolves_to, ...resultNew].flat();
         };
-        getEvolves(result.data.chain).map(elem =>
+        getEvolves(result.data.chain).map((elem) =>
           resultUPD.push([elem.species])
         );
         console.log("result", resultUPD);
@@ -60,7 +60,7 @@ const Evolution = props => {
 
   return (
     <div>
-      <h1>Эволюция</h1>
+      <h1 style={{ textAlign: "center" }}>Coming soon</h1>
     </div>
   );
 };
