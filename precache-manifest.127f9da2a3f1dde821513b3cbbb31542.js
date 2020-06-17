e1d6abd1637025e14ad6e98884082a7551cfc4af
kosst-p <pirozhkovks@yandex.ru>
2020-06-17 16:41:26 +0300

    Updates

---
+++ precache-manifest.127f9da2a3f1dde821513b3cbbb31542.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef3b27d8f172ded6f90f9d8d9102e8b3",
+    "url": "/pokemons-app/index.html"
+  },
+  {
+    "revision": "13553edc10857116f8da",
+    "url": "/pokemons-app/static/css/2.185bf55b.chunk.css"
+  },
+  {
+    "revision": "39e10d79daff6d5e976f",
+    "url": "/pokemons-app/static/css/main.ef4f7a8a.chunk.css"
+  },
+  {
+    "revision": "13553edc10857116f8da",
+    "url": "/pokemons-app/static/js/2.14ff6aa9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokemons-app/static/js/2.14ff6aa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39e10d79daff6d5e976f",
+    "url": "/pokemons-app/static/js/main.0413188a.chunk.js"
+  },
+  {
+    "revision": "a7d3b2aec11013b0004c",
+    "url": "/pokemons-app/static/js/runtime-main.ff8ac803.js"
+  },
+  {
+    "revision": "0334b4cf7d00457f60402c11435a81cf",
+    "url": "/pokemons-app/static/media/pokemon-font.0334b4cf.eot"
+  },
+  {
+    "revision": "0abc39ef711f756f8ad83ef2a8e03c0d",
+    "url": "/pokemons-app/static/media/pokemon-font.0abc39ef.ttf"
+  },
+  {
+    "revision": "752957306a7bb32f0d698d09a167724a",
+    "url": "/pokemons-app/static/media/pokemon-font.75295730.woff2"
+  },
+  {
+    "revision": "d6779c871e189d49817578ce64323508",
+    "url": "/pokemons-app/static/media/pokemon-font.d6779c87.woff"
+  },
+  {
+    "revision": "c25e4fb97d4a96b35322363241d91c84",
+    "url": "/pokemons-app/static/media/pokemonbg.c25e4fb9.jpg"
+  }
+]);
